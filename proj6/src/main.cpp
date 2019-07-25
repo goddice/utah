7ca1356163be5731539c47678104e225461d15f7
Celong Liu <celong.liu@innopeaktech.com>
2019-07-25 13:35:51 -0700

    add bvh acceleration

---
+++ proj6/src/main.cpp
@@ -0,0 +1,66 @@
+//-------------------------------------------------------------------------------
+///
+/// \file       viewport.cpp 
+/// \author     Cem Yuksel (www.cemyuksel.com)
+/// \version    1.2
+/// \date       August 28, 2017
+///
+/// \brief Example source for CS 6620 - University of Utah.
+///
+//-------------------------------------------------------------------------------
+
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <functional>
+#include <thread>
+#include <memory>
+
+#include "render.h"
+
+Node rootNode;
+Camera camera;
+RenderImage renderImage;
+MaterialList materials;
+LightList lights;
+ObjFileList objList;
+Plane thePlane;
+Sphere theSphere;
+std::shared_ptr<std::thread> td;
+
+void BeginRender() {
+    td = std::make_shared<std::thread>(render);
+}
+
+void StopRender() {
+    td->join();
+}
+
+void MonitorRender() {
+    monitor();
+}
+
+void RayTracing() {
+    BeginRender();
+    MonitorRender();
+    StopRender();
+}
+
+int main(int argc, char** argv) {
+/* 	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " scene.xml\n";
+		exit(1);
+	} */
+    LoadScene("proj6/box.xml");
+    
+    std::cout << "#lights: " << lights.size() << std::endl;
+
+    RayTracing();
+    renderImage.SaveImage("proj6/box.png");
+    renderImage.SaveZImage("proj6/box_z.png");
+    return 0;    
+}
--- proj6/src/objects.h
+++ proj6/src/objects.h
@@ -3,8 +3,8 @@
 ///
 /// \file       objects.h 
 /// \author     Cem Yuksel (www.cemyuksel.com)
-/// \version    5.0
-/// \date       September 24, 2015
+/// \version    6.0
+/// \date       September 30, 2015
 ///
 /// \brief Example source for CS 6620 - University of Utah.
 ///
@@ -15,6 +15,7 @@
  
 #include "scene.h"
 #include "cyCodeBase/cyTriMesh.h"
+#include "cyCodeBase/cyBVH.h"
 
 #define EPS 0.001
 
@@ -49,14 +50,18 @@ public:
  
     bool Load(const char *filename)
     {
+        bvh.Clear();
         if ( ! LoadFromFileObj( filename ) ) return false;
         if ( ! HasNormals() ) ComputeNormals();
         ComputeBoundingBox();
+        bvh.SetMesh(this,4);
         return true;
     }
  
 private:
+    cyBVHTriMesh bvh;
     bool IntersectTriangle( const Ray &ray, HitInfo &hInfo, int hitSide, unsigned int faceID ) const;
+    bool TraceBVHNode( const Ray &ray, HitInfo &hInfo, int hitSide, unsigned int nodeID ) const;
 };
  
 //-------------------------------------------------------------------------------
